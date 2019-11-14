be9530caeb327b8e63270a22dee0c4cb05157677
Mihai Sarbulescu <mihai.sarbulescu@gmail.com>
2019-11-14 05:28:05 +0200

    added some other stuff ... CTRL+C event mainly

--- CreateProcessAsUserTest/CreateProcessAsUserTest.cpp
+++ CreateProcessAsUserTest/CreateProcessAsUserTest.cpp
@@ -7,10 +7,30 @@
 #pragma comment(lib, "netapi32.lib")
 #pragma comment(lib, "userenv.lib")
 
-void PrintWin32ErrorToString(DWORD dwErr)
+HANDLE g_hStopEvent = NULL;
+HANDLE g_hProcess = NULL;
+
+bool WINAPI ConsoleHandler(DWORD signal)
+{
+    if (signal == CTRL_C_EVENT ||
+        signal == CTRL_CLOSE_EVENT ||
+        signal == CTRL_BREAK_EVENT ||
+        signal == CTRL_LOGOFF_EVENT ||
+        signal == CTRL_SHUTDOWN_EVENT
+        )
+    {
+        TerminateProcess(g_hProcess, S_OK);
+        SetEvent(g_hStopEvent);
+    }
+
+    return true;
+}
+
+void PrintWin32ErrorToString(LPCWSTR szMessage, DWORD dwErr)
 {
     const int maxSite = 512;
-    LPWSTR szDefaultMessage = L"<< unknown message for this error code >>";
+    const LPCWSTR szFormat = L" hex: 0x%x dec: %d message: %s\n";
+    LPCWSTR szDefaultMessage = L"<< unknown message for this error code >>";
     WCHAR wszMsgBuff[maxSite];
     DWORD dwChars;
     HINSTANCE hInst = NULL;
@@ -30,7 +50,7 @@ void PrintWin32ErrorToString(DWORD dwErr)
         hInst = LoadLibraryW(L"Ntdsbmsg.dll");
         if (!hInst)
         {
-            wprintf(L"%s\n", szDefaultMessage);
+            wprintf(szFormat, dwErr, dwErr, szDefaultMessage);
         }
 
         dwChars = FormatMessageW(
@@ -43,11 +63,14 @@ void PrintWin32ErrorToString(DWORD dwErr)
             nullptr
         );
 
-        FreeLibrary(hInst);
-        hInst = NULL;
+        if (hInst)
+        {
+            FreeLibrary(hInst);
+            hInst = NULL;
+        }
     }
 
-    wprintf(L"%s\n", (dwChars ? wszMsgBuff : szDefaultMessage));
+    wprintf(szFormat, dwErr, dwErr, (dwChars ? wszMsgBuff : szDefaultMessage));
 }
 
 int wmain(int argc, PWCHAR argv[])
@@ -88,8 +111,7 @@ int wmain(int argc, PWCHAR argv[])
     ))
     {
         status = GetLastError();
-        std::wcout << L"ERROR: Cannot logon user with error: " << status << " => ";
-        PrintWin32ErrorToString(status);
+        PrintWin32ErrorToString(L"ERROR: Cannot logon user with error:", status);
         goto cleanup;
     }
 
@@ -116,8 +138,7 @@ int wmain(int argc, PWCHAR argv[])
         if (status != ERROR_SUCCESS)
         {
             status = HRESULT_FROM_WIN32(status);
-            std::wcout << L"ERROR: Cannot find domain controller with error: " << status << " => ";
-            PrintWin32ErrorToString(status);
+            PrintWin32ErrorToString(L"ERROR: Cannot find domain controller with error:", status);
             goto cleanup;
         }
     }
@@ -132,8 +153,7 @@ int wmain(int argc, PWCHAR argv[])
     if (status != ERROR_SUCCESS)
     {
         status = HRESULT_FROM_WIN32(status);
-        std::wcout << L"ERROR: Cannot get user info with error: " << status << " => ";
-        PrintWin32ErrorToString(status);
+        PrintWin32ErrorToString(L"ERROR: Cannot get user info with error:", status);
         goto cleanup;
     }
 
@@ -146,16 +166,14 @@ int wmain(int argc, PWCHAR argv[])
     if (!LoadUserProfileW(hToken, &pProfileInfo))
     {
         status = GetLastError();
-        std::wcout << L"ERROR: Cannot user profile with error: " << status << " => ";
-        PrintWin32ErrorToString(status);
+        PrintWin32ErrorToString(L"ERROR: Cannot user profile with error:", status);
         goto cleanup;
     }
 
     if (!CreateEnvironmentBlock(&pEnvironmentBlock, hToken, false))
     {
         status = GetLastError();
-        std::wcout << L"ERROR: Cannot create environment block with error: " << status << " => ";
-        PrintWin32ErrorToString(status);
+        PrintWin32ErrorToString(L"ERROR: Cannot create environment block with error:", status);
         goto cleanup;
     }
 
@@ -180,28 +198,56 @@ int wmain(int argc, PWCHAR argv[])
     ))
     {
         status = HRESULT_FROM_WIN32(GetLastError());
-        std::wcout << L"ERROR: Cannot create process with error: " << status << " => ";
-        PrintWin32ErrorToString(status);
+        PrintWin32ErrorToString(L"ERROR: Cannot create process with error:", status);
         goto cleanup;
     }
     else
     {
         std::wcout << L"!!! SUCCESS !!! => Waiting (forever) for child porcess to exit ..." << std::endl;
 
-        status = WaitForSingleObject(processInfo.hProcess, INFINITE);
+        g_hProcess = processInfo.hProcess;
+
+        g_hStopEvent = CreateEventW(NULL, true, false, nullptr);
+        if (!g_hStopEvent)
+        {
+            status = GetLastError();
+            PrintWin32ErrorToString(L"ERROR: Failed to create event with error:", status);
+            goto cleanup;
+        }
+
+        if (!SetConsoleCtrlHandler(reinterpret_cast<PHANDLER_ROUTINE>(ConsoleHandler), true))
+        {
+            status = GetLastError();
+            PrintWin32ErrorToString(L"ERROR: Could not set control handler with error:", status);
+            goto cleanup;
+        }
+
+        std::wcout << L"\tAlso waiting for \"CTRL+C\" to (force) terminate the child process and finish the program (just in case it has no UI or you can't see it for some reason) ..." << std::endl;
+        std::wcout << L"\tIf you are using a tool like PsExec.exe (or similar) to start this RunAs tool (program/exe), then, if you press \"CTRL+C\"," << std::endl;
+        std::wcout << L"it will terminate without being able to close the child process in some situations (running under gMSA for example) and so, " << std::endl;
+        std::wcout << L"it might be that you need to kill the child process manually - PID of the child process is: " << processInfo.dwProcessId << std::endl;
+
+        const int waitHandleCount = 2;
+        HANDLE hWaitForHandles[waitHandleCount];
+        hWaitForHandles[0] = processInfo.hProcess;
+        hWaitForHandles[1] = g_hStopEvent;
+
+        status = WaitForMultipleObjects(waitHandleCount, hWaitForHandles, false, INFINITE);
         if (status == WAIT_OBJECT_0)
         {
             if (!GetExitCodeProcess(processInfo.hProcess, &status))
             {
                 status = GetLastError();
-                std::wcout << L"ERROR: Failed to get exit status of child process with error: " << status << " => ";
-                PrintWin32ErrorToString(status);
+                PrintWin32ErrorToString(L"ERROR: Failed to get exit status of child process with error:", status);
             }
             else
             {
-                std::wcout << L"Child process succesfully existed with exit code: " << status << " => ";
-                PrintWin32ErrorToString(status);
+                PrintWin32ErrorToString(L"Child process succesfully existed with exit code:", status);
             }
+        }
+        else if (status == (WAIT_OBJECT_0 + 1))
+        {
+            std::wcout << L"Cancel event (Ctrl+C) was pressed, so it \"killed\" the child process and thus the exit status is irrelevant." << std::endl;
         }
         else
         {
@@ -257,6 +303,12 @@ cleanup:
     {
         DestroyEnvironmentBlock(pEnvironmentBlock);
         pEnvironmentBlock = nullptr;
+    }
+
+    if (g_hStopEvent)
+    {
+        CloseHandle(g_hStopEvent);
+        g_hStopEvent = NULL;
     }
 
     return status;
